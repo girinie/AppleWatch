885bf419b1dcf5d251ab64b1b671751c25c72020
girish <girish@infopro.com.my>
2019-01-03 11:54:38 +0800

    Error applewatch.webview

--- src/ios/app/AppDelegate+applewatch.m
+++ src/ios/app/AppDelegate+applewatch.m
@@ -46,7 +46,7 @@
 - (void) callJavascriptFunctionWhenAvailable:(NSString*)function {
   AppleWatch *appleWatch = [self.viewController getCommandInstance:@"AppleWatch"];
   if (appleWatch.initDone) {
-    [appleWatch.webView stringByEvaluatingJavaScriptFromString:function];
+  //  [appleWatch.webView stringByEvaluatingJavaScriptFromString:function];
   } else {
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 80 * NSEC_PER_MSEC), dispatch_get_main_queue(), ^{
       [self callJavascriptFunctionWhenAvailable:function];
